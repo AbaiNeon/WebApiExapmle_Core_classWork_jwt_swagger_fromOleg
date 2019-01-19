2e77c67b3a374738c9b422de6cab21601b6419eb
ТогузбаевА <abai2010@bk.ru>
2019-01-19 13:49:10 +0600

    Добавьте файлы проекта.

---
+++ DataAccess/SportContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataAccess
+{
+    public class SportContext : DbContext
+    {
+        public SportContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var team = new Team
+            {
+                Id = 1,
+                Name = "Спартак"
+            };
+
+            var player = new Player
+            {
+                Id = 1,
+                FullName = "Роналдо",
+                Number = 7,
+                TeamId = team.Id
+            };
+
+            modelBuilder.Entity<Team>().HasData(team);
+            modelBuilder.Entity<Player>().HasData(player);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ WebApiExample/Startup.cs
@@ -0,0 +1,89 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Services;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Text;
+
+namespace WebApiExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<SportContext>(options => options.UseInMemoryDatabase("InMemorySport"));
+
+            services.AddScoped<UserService>();
+
+            var key = Encoding.ASCII.GetBytes(Configuration["JwtSettings:SecretKey"]);
+            var issuer = Configuration["JwtSettings:Issuer"];
+            var consumer = Configuration["JwtSettings:Consumer"];
+
+            services.AddAuthentication(settings =>
+            {
+                settings.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                settings.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(settings =>
+            {
+                settings.RequireHttpsMetadata = false;
+                settings.SaveToken = true;
+                settings.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Api документация для проекта", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api документация для проекта");
+            });
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
